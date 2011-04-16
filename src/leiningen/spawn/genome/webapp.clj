26457cbdd575a6e0ddfae438967164de2cd1962e
Christopher Redinger <redinger@gmail.com>
2011-04-15 23:46:13 -0400

    Fix dir names with dashes

--- src/leiningen/spawn/genome/webapp.clj
+++ src/leiningen/spawn/genome/webapp.clj
@@ -1,5 +1,6 @@
 (ns leiningen.spawn.genome.webapp
-  (:require [leiningen.spawn.genome-util :as gene]))
+  (:require [leiningen.spawn.genome-util :as gene]
+            [clojure.string :as str]))
 
 (defn- t
   "Utility function for building a local template path"
@@ -9,14 +10,15 @@
 (defn webapp
   "A genome for creating webapps"
   [name]
-  (let [data {"name" name}]
+  (let [data {"name" name}
+        dir-name (str/replace name \- \_)]
     (gene/build-filesystem
      {name {"project.clj" (gene/apply-template (t "project_clj") data)
             "README"      (gene/apply-template (t "README") data)
-            "src"         {name {"server.clj" (gene/apply-template (t "src/server_clj") data)
-                                 "controllers" {"index.clj" (gene/apply-template (t "src/controllers/index_clj") data)}
-                                 "views" {}}}
-            "test"        {name {"test" {"server.clj" (gene/apply-template (t "test/server_clj_test") data)}}}
+            "src"         {dir-name {"server.clj" (gene/apply-template (t "src/server_clj") data)
+                                     "controllers" {"index.clj" (gene/apply-template (t "src/controllers/index_clj") data)}
+                                     "views" {}}}
+            "test"        {dir-name {"test" {"server.clj" (gene/apply-template (t "test/server_clj_test") data)}}}
             "public"      {"robots.txt" (gene/apply-template (t "public/robots_txt") data)
                            "js" {}
                            "css" {}
